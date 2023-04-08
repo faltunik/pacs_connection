150eabaa818e548a21fffd719941fa3c24d695c5
faltunik <niksanat8@gmail.com>
2023-04-08 11:35:16 +0530

    create handler for handling failed upload

--- cstore.py
+++ cstore.py
@@ -1,5 +1,7 @@
 import time
 import json
+import csv
+import os
 from dataclasses import dataclass, field
 from typing import Any
 from pydicom.dataset import Dataset
@@ -56,11 +58,10 @@ class CStore:
         # get All files in folder and for each file call send_c_store and if status is success/failed, create a csv files with file path and status(failed/success)
 
         # so get all files name inside folder
-        import csv
-        import os
         dummy_name = folder_path.split("/")[-1]
         dummy_name = dummy_name.replace(" ", "_")
         count = 0
+        failed = False
         with open(f'upload_results/result_{dummy_name}.csv', mode='w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerow(['File Path', 'Status'])
@@ -76,13 +77,50 @@ class CStore:
                 if status:
                     print('C-STORE request status: 0x{0:04x}'.format(status.Status))
                 else:
+                    failed = True
                     print('Connection timed out, was aborted or received invalid response')
-            
+        return failed
+
+    def handle_failed_request(self, report_path:str)->None:
+        all_success= True
+        updated_rows = []
+        with open(report_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            for idx, row in enumerate(reader):
+                if not row: continue
+                file_path = row[0]
+                if file_path == 'File Path': continue
+                # skip the file if it's already success
+                if row[1] == 'Success':
+                    updated_rows.append(row)
+                    continue
+                print(f"Uploading file: {file_path}")
+                status = self.send_c_store(file_path)
+
+                # If the upload was successful, update the status in the CSV file
+                if status:
+                    row[1] = 'Success'
+                else:
+                    row[1] = 'Failed'
+                    all_success = False
+                updated_rows.append(row)
+
+        # Write the updated row to the CSV file
+        with open(report_path, 'w', newline='') as csv_file:
+            fieldnames = ['File Path', 'Status']
+            writer = csv.DictWriter(csv_file,delimiter=',', fieldnames=fieldnames)
+            writer.writeheader()
+            for row in updated_rows:
+                writer.writerow({'File Path': row[0], 'Status': row[1]})
+        return all_success
+
+          
 
 
 if __name__ == "__main__":
     cstore = CStore("DicomServer.co.uk", 104)
-    cstore.upload_full_study("test_file")
+    #cstore.upload_full_study("test_file")
+    cstore.handle_failed_request("upload_results/result_test_file.csv")
     print('DONE REQUEST, Thanks you!')
 
 
